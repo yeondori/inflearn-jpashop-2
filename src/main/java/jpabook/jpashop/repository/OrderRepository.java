4ef58d088fadc7acf177c07f60caf762ebb1b067
yeondori <yeonseo.jung0@gmail.com>
2023-11-22 15:40:43 +0900

    feat: findAllWithMemberDelivery 페이징 기능 추가(섹션 4)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -96,8 +96,18 @@ public class OrderRepository {
         return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
-                        " join fetch  o.delivery d", Order.class
-                ).getResultList();
+                        " join fetch  o.delivery d", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch  o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 
     public List<Order> findAllWithItem() {
